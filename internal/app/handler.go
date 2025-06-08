1dd2ffb212860b181dbf1bf236b01955857bb2bc
DimKa163 <mitya.martynov.96@gmail.com>
2025-06-08 20:31:51 +0400

    feat: iter1

--- internal/app/handler.go
+++ internal/app/handler.go
@@ -14,16 +14,21 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 		value, err := GetUrl(path)
 		if err != nil {
 			w.WriteHeader(http.StatusBadRequest)
+			return
 		}
 		w.Header().Set("Location", value)
 		w.WriteHeader(http.StatusTemporaryRedirect)
 	case http.MethodPost:
 		defer r.Body.Close()
 		url, err := io.ReadAll(r.Body)
-
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
 		shortUrl, err := CreateShortUrl(string(url), 5)
 		if err != nil {
 			w.WriteHeader(http.StatusInternalServerError)
+			return
 		}
 		body := []byte(fmt.Sprintf("http://%s/%s", r.Host, shortUrl))
 		_, err = w.Write(body)
